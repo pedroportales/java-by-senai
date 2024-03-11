6a7ce570f6de1e4d0c45b7df038167a6aba55d09
pedroportales <pedroportales@riseup.net>
2024-03-11 19:27:07 -0300

    cachorro e gato

---
+++ CachorroGato/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Cachorro cachorro = new Cachorro();
+        Gato gato = new Gato();
+
+        cachorro.emitirSom();
+        gato.emitirSom();
+    }
+}
