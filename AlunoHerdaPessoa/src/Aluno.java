0fae6abb50ee68851f7aa13b37e3d29fc08c8bb5
pedroportales <pedroportales@riseup.net>
2024-03-11 19:10:59 -0300

    Aluno herda pessoa

---
+++ AlunoHerdaPessoa/src/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno extends Pessoa {
+    private Integer matricula;
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula() {
+        this.matricula = matricula;
+    }
+}
---
+++ AlunoHerdaPessoa/src/Pessoa.java
@@ -0,0 +1,20 @@
+public class Pessoa {
+    private String nome;
+    private Integer idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setNome() {
+        this.nome = nome;
+    }
+
+    public void setIdade() {
+        this.idade = idade;
+    }
+}
