6606b53cae41e9ede61408ae7cabebd345500de0
pedroportales <pedroportales@riseup.net>
2024-03-12 13:37:08 -0300

    veiculo carro moto

---
+++ VeiculoCarroMoto/src/Veiculo.java
@@ -0,0 +1,20 @@
+public class Veiculo {
+    private String marca;
+    private String modelo;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setMarca() {
+        this.marca = marca;
+    }
+
+    public void setModelo() {
+        this.modelo = modelo;
+    }
+}
